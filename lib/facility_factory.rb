fb73c46777a8bea8be8c4678724c39a2336d5a3a
Austin Vanloon <wic404learn@gmail.com>
2025-04-07 23:22:38 -0600

    Updated facility factory to include co

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,18 +1,10 @@
 class FacilityFactory
-    def initialize(data)
-      @data = data
-    end
-  
-    def create_facilities
-      @data.map do |facility_data|
-        Facility.new(facility_data)
-      end
-    end
-  end
+    def create_facilities(data)
+      data.map do |facility_data|
+        if facility_data[:state] == "CO"
+          Facility.new({
+            name: facility_data[:dmv_office],
+            address: "#{facility_data[:address_li]} #{facility_data[:address__1]} #{facility_data[:city]} #{facility_data[:state]} #{facility_data[:zip]}",
+            phone: facility_data[:phone]
+          })
 
-
-
-
-
-  
-  #def
