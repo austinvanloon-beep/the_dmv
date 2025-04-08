1b38aac208f2e5182e5c0c4310dec1543598ce1e
Austin Vanloon <wic404learn@gmail.com>
2025-04-07 23:01:16 -0600

    Updated Facility factory class with create_facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,4 +3,9 @@ class FacilityFactory
       @data = data
     end
   
-  
+    def create_facilities
+      @data.map do |facility_data|
+        Facility.new(facility_data)
+      end
+    end
+  end
